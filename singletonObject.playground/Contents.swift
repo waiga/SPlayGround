15fac923c5460fedbea7d16e42d16958ec0cfd0a
waiga <hyang9387@gmail.com>
2022-02-12 23:11:52 +0900

    singleton

--- singletonObject.playground/Contents.swift
+++ singletonObject.playground/Contents.swift
@@ -3,3 +3,40 @@ import UIKit
 class Car {
     var colour = "Red"
 }
+
+let myCar = Car()
+myCar.colour = "Blue"
+
+let yourCar = Car()
+print(yourCar.colour)
+
+print()
+class Car2 {
+    var colour = "Red"
+    static let singletonCar = Car2()
+}
+
+let myCar1 = Car2.singletonCar
+myCar1.colour = "Blue"
+
+let yourCar2 = Car2.singletonCar
+print(yourCar2.colour)
+
+class A {
+    init() {
+        Car2.singletonCar.colour = "Brown"
+    }
+}
+
+class B {
+    init() {
+        print(Car2.singletonCar.colour)
+    }
+}
+
+
+
+let a = A()
+let b = B()
+
+
